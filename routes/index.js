f50d582a58a7eae6103dc36f96d3b759a4c7bd25
Shawn Stern <stern.shawn@gmail.com>
2017-02-21 23:07:53 -0800

    Successfully exposing KeystoneJS API
    -Keystone API now functioning (ie. GET and other requests possible to routes su$
    -Reference this comment for fix on initAPI error that was causing routes to not$

--- routes/index.js
+++ routes/index.js
@@ -24,11 +24,22 @@ keystone.set('500', function(err, req, res, next) {
 
 // Load Routes
 var routes = {
-  views: importRoutes('./views')
+  views: importRoutes('./views'),
+  api: importRoutes('./api'),
 };
 
 // Bind Routes
 exports = module.exports = function(app) {
-  app.get('/', routes.views.index);
+  app.get('/api/post/list', keystone.middleware.api, routes.api.posts.list);
+  app.all('/api/post/create', keystone.middleware.api, routes.api.posts.create);
+  app.get('/api/post/:id', keystone.middleware.api, routes.api.posts.get);
+  app.all('/api/post/:id/update', keystone.middleware.api, routes.api.posts.update);
+  app.get('/api/post/:id/remove', keystone.middleware.api, routes.api.posts.remove);
 
+  // Catch all other rotues and go to index for now
+  app.get('*', routes.views.index);
+  // app.get('/blog/:category?', routes.views.blog);
+  // app.get('/blog/post/:post', routes.views.post);
+  // app.get('/gallery', routes.views.gallery);
+  // app.all('/contact', routes.views.contact);
 }
