5291fc7f5aac0b28801be36fd2d2c96c12ce8aaa
Shawn Stern <stern.shawn@gmail.com>
2017-03-07 16:58:25 -0800

    Updating Blog/reducer.js comments

--- client/app/containers/Blog/reducer.js
+++ client/app/containers/Blog/reducer.js
@@ -1,13 +1,5 @@
 /*
- * HomeReducer
- *
- * The reducer takes care of our data. Using actions, we can change our
- * application state.
- * To add a new action, add it to the switch statement in the reducer function
- *
- * Example:
- * case YOUR_ACTION_CONSTANT:
- *   return state.set('yourStateVariable', true);
+ * BlogReducer
  */
 import { fromJS } from 'immutable';
 
