cdf98ceb36396c27087cb19f484b764ed083fe32
Shawn Stern <stern.shawn@gmail.com>
2017-03-09 16:31:04 -0800

    Updating BlogCard text display
    -Updating cards to include content.brief section above main body text
    -Updating card display to strip down input html to pure text for consistent display. Also, truncating for consistent max-length of preview text.
    -Helper methods/libraries added to support these operations.

--- client/app/components/BlogComponents/BlogCard/index.js
+++ client/app/components/BlogComponents/BlogCard/index.js
@@ -1,26 +1,51 @@
 // Element for displaying blog previews
 import React, { PropTypes } from 'react';
+import he from 'he';
 import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
 import { Button } from 'react-toolbox/lib/button';
 
-const BlogCard = ({ post, date }) => (
-  <Card style={{ width: 'auto' }}>
-    <CardTitle
-      title={post.title}
-      subtitle={`Published on ${date}`}
-    />
-    <CardMedia
-      aspectRatio="wide"
-      image={post.image ? post.image.url : 'https://placeimg.com/800/450/nature'}
-    />
-    <CardText>
-      <p dangerouslySetInnerHTML={{ __html: post.content.extended.html || post.content.markdown.html }} />
-    </CardText>
-    <CardActions>
-      <Button label="Read More..." />
-    </CardActions>
-  </Card>
-);
+// Helper function to strip tags from blog text for consistent display of previews
+const htmlToString = (html) => {
+  const strippedHtml = html.replace(/<[^>]+>/g, '');
+  return he.decode(strippedHtml);
+};
+
+// Truncate string down to a maximum of 'limit' characters and add ellipsis if too long.
+// From there,
+const truncate = (string, limit) => {
+  // Do an immediate return for short strings :)
+  if (string.length <= limit) {
+    return string;
+  }
+  const substring = string.substr(0, limit);
+  // Track back to the end of the last whole word and replace with ellipsis so
+  // that we don't have partially split word at the end...
+  return substring.substr(0, substring.lastIndexOf(' ')).concat('...');
+};
+
+const BlogCard = ({ post, date }) => {
+  const brief = htmlToString(post.content.brief.html || post.content.brief);
+  const summary = truncate(htmlToString(post.content.extended.html || post.content.markdown.html), 250);
+  return (
+    <Card style={{ width: 'auto' }}>
+      <CardTitle
+        title={post.title}
+        subtitle={`Published on ${date}`}
+      />
+      <CardMedia
+        aspectRatio="wide"
+        image={post.image ? post.image.url : 'https://placeimg.com/800/450/nature'}
+      />
+      <CardText>
+        <blockquote dangerouslySetInnerHTML={{ __html: brief }} />
+        <p dangerouslySetInnerHTML={{ __html: summary }} />
+      </CardText>
+      <CardActions>
+        <Button label="Read More..." />
+      </CardActions>
+    </Card>
+  );
+};
 
 BlogCard.propTypes = {
   date: PropTypes.string,
