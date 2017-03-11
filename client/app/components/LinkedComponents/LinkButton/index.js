bb2b0d4ae3b07d8d46e3c72496aa83f6e27fbd04
Shawn Stern <stern.shawn@gmail.com>
2017-03-10 19:38:33 -0800

    Updating LinkButton to properly display destination on hover
    -Modified behavior from connected component with react-redux push() to use react-router Link as a wrapper.

--- client/app/components/LinkedComponents/LinkButton/index.js
+++ client/app/components/LinkedComponents/LinkButton/index.js
@@ -2,24 +2,21 @@
 // push() functionality since react-toolbox doesn't support router by default
 
 import React, { PropTypes } from 'react';
-import { push } from 'react-router-redux';
 import { Button } from 'react-toolbox/lib/button';
-import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+import styles from './styles.scss';
 
 // Use destructuring like a boss
-const LinkButton = ({ label, to, changeRoute }) => (
-  <Button label={label} onClick={() => { changeRoute(to); }} />
+const LinkButton = ({ label, to }) => (
+  <Link to={to}>
+    <Button label={label} className={styles.linkButton} />
+  </Link>
 );
-
-const mapDispatchToProps = (dispatch) => ({
-  changeRoute: (url) => dispatch(push(url)),
-  dispatch,
-});
 
 LinkButton.propTypes = {
   label: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
-  changeRoute: PropTypes.func.isRequired,
 };
 
-export default connect(null, mapDispatchToProps)(LinkButton);
+export default LinkButton;
