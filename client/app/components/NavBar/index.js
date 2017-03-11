c1bdc11e1037dc8456b95d6904929de8edbeda3b
Shawn Stern <stern.shawn@gmail.com>
2017-03-10 18:29:31 -0800

    Removing old comment from NavBar

--- client/app/components/NavBar/index.js
+++ client/app/components/NavBar/index.js
@@ -6,7 +6,6 @@ import LinkButton from '../LinkedComponents/LinkButton';
 // Styles
 import styles from './styles.scss';
 
-// TODO: Replace with a clickable hamburger/etc and have as a slide in side-menu
 const NavBar = () => (
   <div className={styles.bar}>
     <LinkButton label="Home" to="/" />
