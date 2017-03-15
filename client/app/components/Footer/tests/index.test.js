3f17e65a628804c3df44755d6a85409884c23203
Shawn Stern <stern.shawn@gmail.com>
2017-03-15 15:03:21 -0700

    Updating Footer test coverage (75%)

--- client/app/components/Footer/tests/index.test.js
+++ client/app/components/Footer/tests/index.test.js
@@ -5,10 +5,13 @@ import bulma from 'styles/bulma.scss';
 import Footer from '../index';
 
 describe('<Footer />', () => {
+  it('should render the Footer element as <footer>', () => {
+    const renderedComponent = shallow(<Footer />);
+    expect(renderedComponent.find('footer').node).toBeDefined();
+  });
+
   it('should render the copyright notice', () => {
-    const renderedComponent = shallow(
-      <Footer />
-    );
+    const renderedComponent = shallow(<Footer />);
     expect(renderedComponent.contains(
       <p>
         <strong>KeystoneJS + React-Redux</strong> by <a href="https://github.com/stern-shawn">Shawn Stern</a>. The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
