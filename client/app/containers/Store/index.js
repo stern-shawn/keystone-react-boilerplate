3305c6395def4806e942621c6b3308a039d53d78
Shawn Stern <stern.shawn@gmail.com>
2017-03-16 16:17:26 -0700

    Barebones initial implementation of connected Square store items...
    -TODO: API it

--- client/app/containers/Store/index.js
+++ client/app/containers/Store/index.js
@@ -4,14 +4,43 @@ import { createStructuredSelector } from 'reselect';
 import bulma from 'styles/bulma.scss';
 
 export class Store extends Component { // eslint-disable-line
+  componentDidMount() {
+    this.appendNode();
+    this.appendScript();
+  }
+
+  appendNode = () => {
+    const squareStoreNode = document.createElement('a');
+    squareStoreNode.className = 'sq-embed-item';
+    squareStoreNode.setAttribute('href', 'https://squareup.com/market/wergittep/book');
+    squareStoreNode.setAttribute('target', '_blank');
+    this.divNode.appendChild(squareStoreNode);
+  };
+
+  appendScript = () => {
+    // First check to see if it's already been loaded. It won't fire again so we'll need to remove and re-append
+    if (document.getElementById('sq-embed-js')) {
+      const scriptToRemove = document.getElementById('sq-embed-js');
+      scriptToRemove.remove(scriptToRemove);
+    }
+    const squareScript = document.createElement('script');
+    squareScript.id = 'sq-embed-js';
+    squareScript.async = true;
+    squareScript.src = 'https://d2dyi2pd86a6cw.cloudfront.net/market/embed.js';
+    document.body.appendChild(squareScript);
+  }
+
   render() {
     return (
-      <div className={bulma.container}>
-        {'This is a store page'}
-      </div>
+      <div ref={(div) => { this.divNode = div; }} className={bulma.container} />
     );
   }
 }
+
+// <div className={bulma.container}>
+//   <a href="https://squareup.com/market/joshu/black-keychain" className="sq-embed-item">Black Keychain on Square Market</a>
+//   <script src="https://d2dyi2pd86a6cw.cloudfront.net/market/embed.js" id="sq-embed-js" charSet="utf-8"></script>
+// </div>
 
 export const mapDispatchToProps = () => ({
 
