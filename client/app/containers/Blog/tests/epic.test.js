bc983d16f39ffd8e4231018e22cfe9c0ad843029
Shawn Stern <stern.shawn@gmail.com>
2017-03-16 12:50:25 -0700

    Full test coverage of Blog epics, ES6'ing deepEqual import
    -Adding tests for GET_ALL_POSTS and GET_POST_BY_SLUG action listeners.

--- client/app/containers/Blog/tests/epic.test.js
+++ client/app/containers/Blog/tests/epic.test.js
@@ -1,38 +1,72 @@
 import { ActionsObservable } from 'redux-observable';
 import { Observable } from 'rxjs';
+import deepEqual from 'deep-equal';
 import {
-  // getAllBlogPostsEpic,
+  getAllBlogPostsEpic,
   getPageOfPostsEpic,
-  // getBlogPostBySlugEpic,
+  getBlogPostBySlugEpic,
 } from '../epic';
 import {
-  // GET_ALL_POSTS,
+  GET_ALL_POSTS,
   GET_PAGINATED_POSTS,
-  // GET_POST_BY_SLUG,
-  // GET_POSTS_FAILED,
-  // SET_PAGINATED_POSTS,
-  // SET_POST,
-  // SET_POSTS,
+  GET_POST_BY_SLUG,
 } from '../constants';
-import { setPaginatedPosts } from '../actions';
-const deepEqual = require('deep-equal');
+import {
+  setPaginatedPosts,
+  setPost,
+  setPosts,
+} from '../actions';
 
-describe('getAllBlogPostsEpic', () => {
-  it('dispatches the correct action when it is successful', () => {
-    const action$ = ActionsObservable.of(
-      { type: GET_PAGINATED_POSTS, page: 1 }
-    );
-    const response = {
-      posts: {
-        results: [{}, {}],
-      },
-    };
-    const blogApi = { fetchPageOfPosts: () => Observable.of(response) };
+describe('Blog Epics', () => {
+  describe('getPageOfPostsEpic', () => {
+    it('dispatches the correct action and payload when it is successful', () => {
+      const action$ = ActionsObservable.of(
+        { type: GET_PAGINATED_POSTS, page: 1 }
+      );
+      const response = {
+        posts: {
+          results: [{ title: 'My Face' }, { title: 'test-post-1' }],
+        },
+      };
+      const blogApi = { fetchPageOfPosts: () => Observable.of(response) };
 
-    getPageOfPostsEpic(action$, null, { blogApi })
-      .toArray()
-      .subscribe((actions) => {
-        expect(deepEqual(actions, [setPaginatedPosts(response.posts)])).toBe(true);
-      });
+      getPageOfPostsEpic(action$, null, { blogApi })
+        .toArray()
+        .subscribe((actions) => {
+          expect(deepEqual(actions, [setPaginatedPosts(response.posts)])).toBe(true);
+        });
+    });
+  });
+
+  describe('getAllBlogPostsEpic', () => {
+    it('dispatches the correct action and payload when it is successful', () => {
+      const action$ = ActionsObservable.of({ type: GET_ALL_POSTS });
+      const response = {
+        posts: [{ title: 'My Face' }, { title: 'test-post-1' }, { title: 'hello world' }],
+      };
+      const blogApi = { fetchAllFullPosts: () => Observable.of(response) };
+
+      getAllBlogPostsEpic(action$, null, { blogApi })
+        .toArray()
+        .subscribe((actions) => {
+          expect(deepEqual(actions, [setPosts(response.posts)])).toBe(true);
+        });
+    });
+  });
+
+  describe('getBlogPostBySlugEpic', () => {
+    it('dispatches the correct action and payload when it is successful', () => {
+      const action$ = ActionsObservable.of({ type: GET_POST_BY_SLUG, slug: 'my-face' });
+      const response = {
+        post: { title: 'My face' },
+      };
+      const blogApi = { fetchPostBySlug: () => Observable.of(response) };
+
+      getBlogPostBySlugEpic(action$, null, { blogApi })
+        .toArray()
+        .subscribe((actions) => {
+          expect(deepEqual(actions, [setPost(response.post)])).toBe(true);
+        });
+    });
   });
 });
