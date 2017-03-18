ae80b08729f1923f8506edc065b1fb8de13f7a42
Shawn Stern <stern.shawn@gmail.com>
2017-03-17 18:21:05 -0700

    Updating footer

--- client/app/components/Footer/index.js
+++ client/app/components/Footer/index.js
@@ -6,7 +6,7 @@ const Footer = () => (
     <div className={bulma.container}>
       <div className={`${bulma.content} ${bulma['has-text-centered']}`}>
         <p>
-          <strong>KeystoneJS + React-Redux</strong> by <a href="https://github.com/stern-shawn">Shawn Stern</a>. The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+          <strong>KeystoneJS + React-Redux + RxJS</strong> by <a href="https://github.com/stern-shawn">Shawn Stern</a>. The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
         </p>
         <p>
           <a className={bulma.icon} href="https://github.com/stern-shawn/keystone-react-boilerplate">
