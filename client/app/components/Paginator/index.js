459554702688810b792ab851e0d6d791400bb674
Shawn Stern <stern.shawn@gmail.com>
2017-03-30 14:29:16 -0700

    Beginning of procedural generation of paginator links
    -Basic logic established for when there are a low number of pages to render to the paginator, generate links instead of hardcoded.
    -Lowering posts per page returned by API for easier testing/updating max pages for better upper limit.

--- client/app/components/Paginator/index.js
+++ client/app/components/Paginator/index.js
@@ -7,21 +7,36 @@ import styles from './styles.scss';
 // Components
 // import LinkButton from '../LinkedComponents/LinkButton';
 
-const Paginator = ({ currPage, numPages }) => (
-  <nav className={`${bulma.pagination} ${bulma['is-centered']} ${bulma['is-medium']} ${styles.paginator}`}>
-    {numPages < 2 ? null : <a className={`${bulma['pagination-previous']}`}>Previous</a>}
-    {numPages < 2 ? null : <a className={`${bulma['pagination-next']}`}>Next Page</a>}
-    <ul className={`${bulma['pagination-list']}`}>
-      <li><a className={`${bulma['pagination-link']}`}>1</a></li>
-      <li><span className={`${bulma['pagination-ellipsis']}`}>&hellip;</span></li>
-      <li><a className={`${bulma['pagination-link']}`}>45</a></li>
-      <li><a className={`${bulma['pagination-link']} ${bulma['is-current']}`}>46</a></li>
-      <li><a className={`${bulma['pagination-link']}`}>47</a></li>
-      <li><span className={`${bulma['pagination-ellipsis']}`}>&hellip;</span></li>
-      <li><a className={`${bulma['pagination-link']}`}>86</a></li>
-    </ul>
-  </nav>
-);
+const Paginator = ({ currPage, numPages }) => {
+  let pageRange = [];
+  let pageControls;
+
+  if (numPages < 5) {
+    // Build up an array of the page indexes from 1...numPages, convert to li's
+    pageRange = Array(numPages).fill(0).map((e, i) => i + 1);
+    pageControls = pageRange.map((val) =>
+      <li key={val}>
+        <a className={`${bulma['pagination-link']}`}>{val}</a>
+      </li>
+    );
+  }
+
+  return (
+    <nav className={`${bulma.pagination} ${bulma['is-centered']} ${bulma['is-medium']} ${styles.paginator}`}>
+      <a className={`${bulma['pagination-previous']}`}>Previous</a>
+      <a className={`${bulma['pagination-next']}`}>Next Page</a>
+      <ul className={`${bulma['pagination-list']}`}>
+        {pageControls}
+        {/*<li><a className={`${bulma['pagination-link']}`}>1</a></li>
+        <li><span className={`${bulma['pagination-ellipsis']}`}>&hellip;</span></li>
+        <li><a className={`${bulma['pagination-link']}`}>45</a></li>
+        <li><a className={`${bulma['pagination-link']} ${bulma['is-current']}`}>46</a></li>
+        <li><a className={`${bulma['pagination-link']}`}>47</a></li>
+        <li><span className={`${bulma['pagination-ellipsis']}`}>&hellip;</span></li>
+        <li><a className={`${bulma['pagination-link']}`}>86</a></li>*/}
+      </ul>
+    </nav>);
+};
 
 Paginator.propTypes = {
   currPage: PropTypes.number,
